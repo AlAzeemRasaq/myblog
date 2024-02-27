48f7c393ab8a05cc68c905fa98dc0a35c1946f15
Al-Azeem Abdul Rasaq <93706390+AlAzeemRasaq@users.noreply.github.com>
2024-02-27 12:08:36 +0000

    Minor updates to index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,12 @@
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                    Gamers don't die. They respawn.
                 </h1>
                 <a 
                     href="/blog"
                     class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
+                    Enter the blog.
                 </a>
             </div>
         </div>
@@ -18,7 +18,7 @@
 
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+            <img src="https://cdn.mos.cms.futurecdn.net/WUQFsuWeup4yVTTivCMXKe.jpg" width="700" alt="">
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
