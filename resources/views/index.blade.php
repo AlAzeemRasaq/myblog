1f714f6ac5e7b9c34e3135bdf915add5cd753ce0
Al-Azeem Abdul Rasaq <93706390+AlAzeemRasaq@users.noreply.github.com>
2024-03-14 22:04:30 +0000

    Updated index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
+<style>
+    #map
+    {
+        width:1280px;
+        height:720px;
+        border:thin solid #ccc;
+    }
+</style>
+
 @section('content')
     <div class="background-image grid grid-cols-1 m-auto">
         <div class="flex text-gray-100 pt-10">
@@ -9,16 +18,16 @@
                 </h1>
                 <a 
                     href="/blog"
-                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    class="text-center bg-gray-50 text-green-700 py-2 px-4 font-bold text-xl uppercase">
                     Enter the blog.
                 </a>
             </div>
         </div>
     </div>
 
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200 bg-grey text-white">
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200 bg-gray text-white">
         <div>
-            <img src="https://cdn.mos.cms.futurecdn.net/WUQFsuWeup4yVTTivCMXKe.jpg" width="700" alt="">
+            <img src="https://cdn.mos.cms.futurecdn.net/WUQFsuWeup4yVTTivCMXKe.jpg" width="2500" alt="">
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
@@ -31,27 +40,18 @@
             </p>
 
             <body>
-                <iframe src="https://discord.com/widget?id=342761229544194048&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
+                <iframe src="https://discord.com/widget?id=342761229544194048&theme=dark" width="600" height="500" allowtransparency="true" frameborder="0"></iframe>
             </body>
         </div>
     </div>
 
     <div class="text-center p-15 bg-black text-white">
         <h2 class="text-2xl pb-5 text-l"> 
-            I'm an expert in...
+            This blog is brought to you by...
         </h2>
 
         <span class="font-extrabold block text-4xl py-1">
-            Ux Design
-        </span>
-        <span class="font-extrabold block text-4xl py-1">
-            Project Management
-        </span>
-        <span class="font-extrabold block text-4xl py-1">
-            Digital Strategy
-        </span>
-        <span class="font-extrabold block text-4xl py-1">
-            Backend Development
+            Al-Azeem Abdul Rasaq
         </span>
     </div>
 
@@ -61,27 +61,30 @@
         </span>
 
         <h2 class="text-4xl font-bold py-10">
-            Recent Posts
+            Map of Gamers
         </h2>
 
         <p class="m-auto w-4/5 text-gray-500">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos temporibus quaerat facere consectetur qui.
         </p>
+
+        <!-- Google Maps -->
+        <div id=map></div>
+        <script src=https://maps.googleapis.com/maps/api/js?key=AIzaSyA7t2cRC1VnrMEJq59mYMSyjwdmMejdoOE&loading=async&callback=loadMap&libraries=marker></script>       		
     </div>
 
     <div class="sm:grid grid-cols-2 w-4/5 m-auto">
         <div class="flex bg-yellow-700 text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
                 <span class="uppercase text-xs">
-                    PHP
+                    Map of Gamers
                 </span>
 
                 <h3 class="text-xl font-bold py-10">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
-                </h3>
+                </h3>     		
 
-                <a 
-                    href=""
+                <a href=""
                     class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
                     Find Out More
                 </a>
