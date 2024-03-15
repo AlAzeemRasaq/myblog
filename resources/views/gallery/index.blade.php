f855a77b82d79cf487fbc6595b4533876b41fa9a
Al-Azeem Abdul Rasaq <93706390+AlAzeemRasaq@users.noreply.github.com>
2024-03-15 23:19:54 +0000

    gallery has been heavily updated

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -56,105 +56,105 @@
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://vtbroot.s3.amazonaws.com/ROCCAT_Chromatic_Wallpaper_All-Resolutions/ROCCAT_Chromatic_Wallpaper_1920x1080.png" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://r4.wallpaperflare.com/wallpaper/840/247/683/nzxt-pucci-pc-gaming-hd-wallpaper-99a0084d515a6d9bd647d89f20d1467d.jpg" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://wallpapercave.com/wp/wp12820392.jpg" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://www.pcspecialist.co.uk/forums/attachments/image1-jpeg.31241/" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://gaming.lenovo.com/cfs-file/__key/communityserver-blogs-components-weblogfiles/00-00-00-00-93/6305.Wallpaper_5F00_LOQ_5F00_ON.png" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://r4.wallpaperflare.com/wallpaper/845/713/172/nvidia-wallpaper-69d0782d418addeb3677584f1021c67d.jpg" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://media.steelseriescdn.com/blog/posts/official-steelseries-wallpapers/0ce1798a291d43ebadfb4bc4a35fbddc.webp" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://cwsmgmt.corsair.com/landing/wallpaper/img/CORSAIR_2020_Wallpaper_06.jpg" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://wallpapers-fenix.eu/full/181201/114424584.jpg" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://preview.redd.it/up16rwmyfwa71.png?auto=webp&s=02d8997d248f5b3af322bfbaeb1c3cc344305c25" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://images5.alphacoders.com/130/1306174.jpeg" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://i.redd.it/since-you-guys-ask-for-more-victus-inspired-wallpapers-ive-v0-kgkpbxyeehl91.png?width=3840&format=png&auto=webp&s=713392fb062d603483f4ea8c48183b08ce99a133" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://preview.redd.it/zuvaozztcll41.png?width=2560&format=png&auto=webp&s=dabe1fa5d827c81cd6c5ca0826c2e2762491cbd9" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://global.aorus.com/upload/Downloads/F_20210311164347axzz2J.JPG" />
                 </div>
               </div>
               <div class="flex w-1/3 flex-wrap">
                 <div class="w-full p-1 md:p-2">
                   <img
                     class="block h-full w-full rounded-lg object-cover object-center"
-                    src="https://storage-asset.msi.com/global/picture/news/2019/monitor/msi-mecha-squad-desktop-monitor-mecha-squad-wallpaper-960x540.jpg" />
+                    src="https://global.aorus.com/upload/Downloads/F_20210108165071PnmTna.JPG" />
                 </div>
               </div>
               
